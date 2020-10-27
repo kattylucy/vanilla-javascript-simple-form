57709ae2d1b0000be03d316537da795649a89b68
kattylucy <kattybarroso1321@gmail.com>
2020-10-26 17:06:57 -0700

    added validation

--- app.js
+++ app.js
@@ -53,6 +53,19 @@ class UI{
             target.parentElement.parentElement.remove();
         }
     }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
 }
 
 // STORE CLASS: HANDLE STORAGE (local storage)
@@ -69,8 +82,13 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    const book = new Book(title, author, isbn);
-    UI.addBookToList(book);
+    if(title == '' || author == '' || isbn == ''){
+        UI.showAlert('Fields are empty', 'danger')
+    }else{
+        const book = new Book(title, author, isbn);
+        UI.addBookToList(book);
+    }
+  
 
     //clear fields
     UI.clearFields();
